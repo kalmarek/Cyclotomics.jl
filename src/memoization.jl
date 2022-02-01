4a7217d164eaf1fd708a4b4eba6e11eae259e36b
Marek Kaluba <kalmar@amu.edu.pl>
2022-02-01 18:03:18 +0100

    update deps dropping pre julia-1.6

--- src/memoization.jl
+++ src/memoization.jl
@@ -1,19 +1,12 @@
 import Memoize
 import LRUCache
 
-@static if VERSION >= v"1.2.0"
-    Memoize.@memoize LRUCache.LRU{
-        Tuple{Int},
-        Tuple{BitSet,ForbiddenResidues{Int}},
-    }(
-        maxsize = 10000,
-    ) zumbroich_viacomplement(n::Int) =
-        zumbroich_viacomplement(n, Primes.factor(n))
+Memoize.@memoize LRUCache.LRU{Tuple{Int},Tuple{BitSet,ForbiddenResidues{Int}}}(
+    maxsize = 10000,
+) zumbroich_viacomplement(n::Int) = zumbroich_viacomplement(n, Primes.factor(n))
 
-    Memoize.@memoize LRUCache.LRU{
-        Tuple{Int},
-        BitSet,
-    }(
-        maxsize = 10000,
-    ) _coprimes(n::Int) = BitSet(i for i in 2:n if gcd(i, n) == 1)
+Memoize.@memoize LRUCache.LRU{Tuple{Int},BitSet}(maxsize = 10000) function _coprimes(
+    n::Int,
+)
+    return BitSet(i for i in 2:n if gcd(i, n) == 1)
 end
